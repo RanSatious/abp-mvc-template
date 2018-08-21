6819a211e84158d62aa31a3996b2708e145d31a1
satious <383902558@qq.com>
2018-08-21 14:32:19 +0800

    [home] main page layout

--- src/MyCompany.MyProject.Web/Assets/app/main.js
+++ src/MyCompany.MyProject.Web/Assets/app/main.js
@@ -1,4 +1,7 @@
-define(['jquery', 'abp/abp', 'depend!apiService[abp/abp]', 'depend!scriptService[abp/abp]'], function($, abp) {
+define(['jquery', 'abp/abp', 'lay!element', 'depend!apiService[abp/abp]', 'depend!scriptService[abp/abp]'], function(
+  $,
+  abp
+) {
   console.log('main loaded');
   return {
     $: $,
