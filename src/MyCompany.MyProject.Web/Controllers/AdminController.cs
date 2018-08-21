4da27d8ab81874ac276a902371b3219c552f22e1
satious <383902558@qq.com>
2018-08-21 16:57:53 +0800

    [page] add users, roles page

---
+++ src/MyCompany.MyProject.Web/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Abp.Web.Mvc.Authorization;
+using MyCompany.MyProject.Authorization;
+
+namespace MyCompany.MyProject.Web.Controllers
+{
+    public class AdminController : MyProjectControllerBase
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AbpMvcAuthorize(PermissionNames.Pages_Administration_Users)]
+        public ActionResult Users()
+        {
+            return View();
+        }
+
+        [AbpMvcAuthorize(PermissionNames.Pages_Administration_Roles)]
+        public ActionResult Roles()
+        {
+            return View();
+        }
+    }
+}
