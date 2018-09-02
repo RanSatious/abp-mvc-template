08a7265c09006cd2b4897c8e6f8c7bd2f1921ce1
RanSatious <383902558@qq.com>
2018-09-02 15:44:03 +0800

    [project] 添加generator工具

---
+++ src/Tools/MyCompany.MyProject.Generator/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace MyCompany.MyProject.Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(12, 205);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(260, 44);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "确定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnPath
+            // 
+            this.btnPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPath.Location = new System.Drawing.Point(11, 51);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(260, 25);
+            this.btnPath.TabIndex = 3;
+            this.btnPath.Text = "目录";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(12, 24);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(259, 21);
+            this.txtPath.TabIndex = 4;
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.Location = new System.Drawing.Point(11, 178);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(260, 21);
+            this.txtName.TabIndex = 5;
+            this.txtName.Text = "MyProject.MyName";
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Project Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form1";
+            this.Text = "Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ src/Tools/MyCompany.MyProject.Generator/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCompany.MyProject.Generator
+{
+    public partial class Form1 : Form
+    {
+        private string _srcPath;
+        private string _targetPath;
+        private string _defaultSln;
+        private string _defaultProject;
+        private string _targetSln;
+        private string _targetProject;
+        private List<string> _ignorePaths;
+        private List<string> _ignoreTypes;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnOK.Enabled = false;
+
+            _srcPath = ConfigurationManager.AppSettings["SrcPath"];
+            _defaultSln = ConfigurationManager.AppSettings["DefaultSln"];
+            _defaultProject = ConfigurationManager.AppSettings["DefaultProject"];
+            _ignorePaths = ConfigurationManager.AppSettings["IgnorePaths"].Split('|').ToList();
+            _ignoreTypes = ConfigurationManager.AppSettings["IgnoreTypes"].Split('|').ToList();
+        }
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            var result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            // check path
+            if (!this.checkPath())
+            {
+                return;
+            }
+            this._targetPath = txtPath.Text.Trim();
+            // check name
+            if (!this.checkName())
+            {
+                return;
+            }
+            this._targetSln = txtName.Text.Trim();
+
+            this._targetProject = this._targetSln.Split('.').ToList().Last();
+            this.process();
+        }
+
+        private  void process()
+        {
+            try
+            {
+                // copy and replace
+                this.copyFiles(this._srcPath);
+                MessageBox.Show("完成");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private void copyFiles(string path)
+        {
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                // todo: log
+                Console.WriteLine($"copy file {file}");
+
+                // todo: check file
+                if (_ignoreTypes.IndexOf(file.Substring(file.LastIndexOf(".") + 1)) >= 0)
+                {
+                    File.Copy(file, file.Replace(this._srcPath, this._targetPath).Replace(_defaultSln, _targetSln).Replace(_defaultProject, _targetProject));
+                }
+                else
+                {
+                    var content = File.ReadAllText(file).Replace(_defaultSln, _targetSln).Replace(_defaultProject, _targetProject);
+                    File.WriteAllText(file.Replace(this._srcPath, this._targetPath).Replace(_defaultSln, _targetSln).Replace(_defaultProject, _targetProject), content);
+                }
+            }
+
+            var subPaths = Directory.GetDirectories(path);
+            foreach (var subPath in subPaths)
+            {
+                // todo: log
+                Console.WriteLine($"copy path {subPath}");
+
+                var lastPath = subPath.ToLower().Substring(subPath.LastIndexOf("\\") + 1);
+                if (_ignorePaths.IndexOf(lastPath) >= 0)
+                {
+                    continue;
+                }
+
+                // check path
+                var targetSubPath = subPath.Replace(this._srcPath, this._targetPath).Replace(_defaultSln, _targetSln).Replace(_defaultProject, _targetProject);
+                if (!Directory.Exists(targetSubPath))
+                {
+                    Directory.CreateDirectory(targetSubPath);
+                }
+                this.copyFiles(subPath);
+            }
+        }
+
+        private bool checkPath()
+        {
+            try
+            {
+                var path = txtPath.Text.Trim();
+                if (!Directory.Exists(path))
+                {
+                    var result = MessageBox.Show("目录不存在，是否创建", "确认", MessageBoxButtons.OKCancel);
+                    if (result != DialogResult.OK)
+                    {
+                        return false;
+                    }
+                    Directory.CreateDirectory(path);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                // todo: log error
+                MessageBox.Show($"创建目录失败, {e.Message}");
+                return false;
+            }
+        }
+
+        private bool checkName()
+        {
+            var name = txtName.Text.Trim();
+            var reg = new Regex("^[a-zA-Z]([a-zA-Z_.]*[a-zA-Z]|[a-zA-Z]*)$");
+            if (!reg.IsMatch(name))
+            {
+                MessageBox.Show("工程只能包含字母、'.'，且不能以'.'开头和结尾");
+            }
+            return true;
+        }
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+            btnOK.Enabled = this.isOk();
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            btnOK.Enabled = this.isOk();
+        }
+
+        private bool isOk()
+        {
+            return !string.IsNullOrEmpty(txtPath.Text.Trim()) && !string.IsNullOrEmpty(txtName.Text.Trim());
+        }
+    }
+}
