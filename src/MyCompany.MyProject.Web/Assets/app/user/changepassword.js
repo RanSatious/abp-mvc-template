8e1ccc2316c43364ed19cbe7edbe16e80c346e03
Administrator <Administrator@USER-20180104HE>
2018-09-11 12:50:24 +0800

    [Web/Accets] 删除多余代码

--- src/MyCompany.MyProject.Web/Assets/app/user/changepassword.js
+++ src/MyCompany.MyProject.Web/Assets/app/user/changepassword.js
@@ -1,73 +1,32 @@
-﻿define([
-    'main',
-    'lay!form',
-    'lay!layer'], function (main) {
-        var form = layui.form;
-        var layer = layui.layer;
-        var userService = abp.services.app.user;
-        var usersessionService = abp.services.app.session;
-        var modal = {
-            index: 0,
-            model: {},
-            callback: null,
-            roles: null,
-            init: function () {
-                usersessionService.getCurrentLoginInformations().then(function (res) {
-                    modal.model = res.user;
-                });
-                modal.initForm();
-            },
-            initForm: function () {
-                form.on('submit(changesubmit)', function (data) {
-                    console.log('data', data);
-                    var action = userService.changePassword
-                    action(modal.normalize(data.field)).then(function (result) {
-                        layer.close(modal.index);
-                        if (result.succeeded === true) {
-                            layer.msg('密码修改成功', {
-                                time: 2000, //20s后自动关闭
-                                btn: ['好的'],
-                                icon: 1
-                            });
-                        }
-                        else {
-                            layer.msg('密码修改失败', {
-                                time: 2000, //20s后自动关闭
-                                btn: ['好的'],
-                                icon: 2
-                            });
-                        }
-                    });
-                    return false;
-                });
+﻿define(['main', 'lay!form', 'lay!layer'], function(main) {
+  var form = layui.form;
+  var userService = abp.services.app.user;
+  var modal = {
+    model: {},
+    init: function() {
+      modal.initForm();
+    },
+    initForm: function() {
+      form.on('submit(changesubmit)', function(data) {
+        var action = userService.changePassword;
+        action(data.field).then(function(result) {
+          if (result.succeeded) {
+            abp.notify.success('密码修改成功');
+          } else {
+            abp.notify.success('密码修改失败');
+          }
+        });
+        return false;
+      });
 
-                form.verify({
-                    confirm: function (value, item) {
-                        // bad way
-                        if (value != $('input[name=newPassword]').val()) {
-                            return '密码不一致';
-                        }
-                    }
-                });
-            },
-            normalize: function (data) {
-                var item = Object.assign({ isActive: true, surname: data.name, roleNames: [] }, modal.model, data);
-                // clear roles
-                item.roleNames.length = 0;
-                // reset roles
-                var roleProps = [];
-                for (var key in item) {
-                    var match = key.match(/role\[(.*)\]$/);
-                    if (match && item[key] == 'on') {
-                        item.roleNames.push(match[1]);
-                        roleProps.push(key);
-                    }
-                }
-                roleProps.forEach(function (key) {
-                    delete item[key];
-                });
-                return item;
-            }
-        };
-        modal.init();
-    })
+      form.verify({
+        confirm: function(value, item) {
+          if (value != $('input[name=newPassword]').val()) {
+            return '密码不一致';
+          }
+        }
+      });
+    }
+  };
+  modal.init();
+});
--- src/MyCompany.MyProject.Web/Assets/app/user/personalInfo.js
+++ src/MyCompany.MyProject.Web/Assets/app/user/personalInfo.js
@@ -1,38 +1,21 @@
-﻿define(['main', 'dayjs','lay!form'], function (main,dayjs) {
-    var $ = layui.$;
-    var form = layui.form;
-    var userService = abp.services.app.user;
-    var usersessionService = abp.services.app.session;
-    var roleService = abp.services.app.role;
-    var modal = {
-        model: {},
-        roles: [],
-        init: function () {
-            modal.model=usersessionService.getCurrentLoginInformations({}).then(function (res) {
-                userService.getPersonalInfo({ UserName: res.user.userName }).then(function (data) {
-                    data.items[0].creationTime = dayjs(data.items[0].creationTime).format('YYYY-MM-DD HH:mm:ss');;
-                    data.items[0].lastLoginTime = dayjs(data.items[0].lastLoginTime).format('YYYY-MM-DD HH:mm:ss');;
-                    console.log('data.items[0]', data.items[0].roles)
-                    form.val('form-personal', data.items[0]);
-                    data.items[0].roles.forEach(function (d, index) {
-                        $('#personal-roles').append(`<input type="checkbox" name="role[${d[1]}]" lay-skin="primary" title="${d[2]}" disabled>`);
-                    });
-                    var roles = {};
-                    data.items[0].roles.forEach(function (d) {
-                        roles['role[' + d[1] + ']'] = true;
-                    });
-                    form.val('form-personal', roles);
-                });
-            });
-        },
-        initForm: function () {
-            let models = modal.normalizemodel();
-            let roless = modal.normalizeroles();
-            form.val('form-personal', models);
-            roless.forEach(function (d, index) {
-                $('#personal-roles').append(`<input type="checkbox" name="role[${d.name}]" lay-skin="primary" title="${d.displayName}" chencked="" disabled>`);
-            });
-        }
+﻿define(['main', 'dayjs', 'lay!form'], function(main, dayjs) {
+  var $ = layui.$;
+  var form = layui.form;
+  var userService = abp.services.app.user;
+  var usersessionService = abp.services.app.session;
+  var modal = {
+    init: function() {
+      userService.getPersonalInfo({}).then(function(data) {
+        data.items[0].creationTime = dayjs(data.items[0].creationTime).format('YYYY-MM-DD HH:mm:ss');
+        data.items[0].lastLoginTime = dayjs(data.items[0].lastLoginTime).format('YYYY-MM-DD HH:mm:ss');
+        let temp = [];
+        data.items[0].roles.forEach(function(d) {
+          temp.push(d[2]);
+        });
+        data.items[0].roles = temp.join(',');
+        form.val('form-personal', data.items[0]);
+      });
     }
-    modal.init();
-})
+  };
+  modal.init();
+});
