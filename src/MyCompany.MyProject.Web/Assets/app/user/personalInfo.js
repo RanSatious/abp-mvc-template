b2349adc280297cdf41de46fc2dc22d03a8b10f8
Administrator <Administrator@USER-20180104HE>
2018-09-14 09:23:05 +0800

    [Web] 试图页面添加Id，并隐藏，添加表单提交按钮；js文件田间表单提交按钮点击事件

--- src/MyCompany.MyProject.Web/Assets/app/user/personalInfo.js
+++ src/MyCompany.MyProject.Web/Assets/app/user/personalInfo.js
@@ -4,6 +4,9 @@
   var userService = abp.services.app.user;
   var data = {
     init: function() {
+      data.initForm();
+    },
+    initForm: function() {
       userService.getPersonalInfo().then(function(res) {
         res.items[0].creationTime = dayjs(res.items[0].creationTime).format('YYYY-MM-DD HH:mm:ss');
         res.items[0].lastLoginTime = dayjs(res.items[0].lastLoginTime).format('YYYY-MM-DD HH:mm:ss');
@@ -14,6 +17,41 @@
         res.items[0].roles = temp.join(',');
         form.val('form-personal', res.items[0]);
       });
+      form.on('submit(updateUser)', function(result) {
+        var action = userService.updatePrsonalInfo;
+        action(data.normalize(result.field))
+          .then(function(resultres) {
+            console.log('resultres', resultres);
+            if (resultres.succeeded) {
+              abp.notify.success('个人信息修改成功');
+            } else {
+              abp.notify.error(resultres.errors[0]);
+            }
+          })
+          .catch(function(e) {
+            utils.loginOvertime();
+          });
+        return false;
+      });
+    },
+    normalize: function(result) {
+      var res = Object.assign(
+        {
+          creationTime: result.creationTime,
+          emailAddress: result.emailAddress,
+          id: result.id,
+          isActive: result.isActive,
+          lastLoginTime: result.lastLoginTime,
+          name: result.name
+        },
+        result
+      );
+      if (res.isActive == 'on') {
+        res.isActive = true;
+      } else {
+        res.isActive = false;
+      }
+      return res;
     }
   };
   data.init();
