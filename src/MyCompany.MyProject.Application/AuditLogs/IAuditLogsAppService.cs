42e501cf46c4d223250d1e5b7a4110ba6616bf32
Administrator <Administrator@USER-20180104HE>
2018-09-17 13:24:30 +0800

    [Application] 添加日志模块，添加日志实体类，添加查询日志的方法，添加输入查询日志实体

---
+++ src/MyCompany.MyProject.Application/AuditLogs/IAuditLogsAppService.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using MyCompany.MyProject.AuditLogs.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany.MyProject.AuditLogs
+{
+    public interface IAuditLogsAppService : IApplicationService
+    {
+        Task<PagedResultDto<AuditLogsDto>> Get(GetAuditLogInput input);
+    }
+}
